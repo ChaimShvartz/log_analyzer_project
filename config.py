0851a183d6d503171917b3fc45527bb249825ec6
Chaim <c0507634153@gmail.com>
2026-02-06 01:32:56 +0200

    sort the function by the correct files

---
+++ config.py
@@ -0,0 +1,5 @@
+DATA_PATH = 'network_traffic.log'
+INTERNAL_ADDRESSES = ('192.168', '10')
+SENSITIVE_PORT = ['22', '23', '3389']
+PACKET_LARGE = 5000
+gloss = {'timestamp': 0, 'source_ip': 1, 'dest_ip': 2, 'port': 3, 'protocol': 4, 'size': 5}
