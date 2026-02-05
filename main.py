9c9f1187c854fbec78489c5bf5c2305d8e0f8a20
Chaim <c0507634153@gmail.com>
2026-02-05 16:44:08 +0200

    add function that returns unusual size ports

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 DATA_PATH = 'network_traffic.log'
 INTERNAL_ADDRESSES = ('192.168', '10')
 SENSITIVE_PORT = ['22', '23', '3389']
+PACKET_LARGE = 5000
 
 def load_data(data_path) -> list[list[str]] | None:
     try:
@@ -17,7 +18,11 @@ def get_external_addresses(data):
 def get_sensitive_port(data):
     return [line for line in data if line[3] in SENSITIVE_PORT]
 
+def get_unusual_size_ports(data):
+    return [line for line in data if int(line[5]) > PACKET_LARGE]
+
 if __name__ == '__main__':
     # print(load_data(DATA_PATH))
     # print(get_external_addresses(load_data(DATA_PATH)))
-    print(get_sensitive_port(load_data(DATA_PATH)))
+    # print(get_sensitive_port(load_data(DATA_PATH)))
+    print(get_unusual_size_ports(load_data(DATA_PATH)))
