742542fb7b7fcbfe6bc8bb0c088f76d3536c867b
Chaim <c0507634153@gmail.com>
2026-02-05 16:35:13 +0200

    add function that returns all external IP addresses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 
 DATA_PATH = 'network_traffic.log'
+INTERNAL_ADDRESSES = ('192.168', '10')
 
 def load_data(data_path) -> list[list[str]] | None:
     try:
@@ -9,6 +10,10 @@ def load_data(data_path) -> list[list[str]] | None:
     except Exception as e:
         print(e)
 
+def get_external_addresses(data):
+    return [line[1] for line in data if not line[1].startswith(INTERNAL_ADDRESSES)]
+
 
 if __name__ == '__main__':
-    print(load_data(DATA_PATH))
+    # print(load_data('DATA_PATH'))
+    print(get_external_addresses(load_data(DATA_PATH)))
