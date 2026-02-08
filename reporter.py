21282b6c54b71bea6904e27b19b404991293b1db
Chaim <c0507634153@gmail.com>
2026-02-08 11:51:40 +0200

    add function that returns dict which maps ports to protocols

--- reporter.py
+++ reporter.py
@@ -5,4 +5,7 @@ from reader import load_data
 def get_counter_dict(data):
     return {line[gloss["source_ip"]]: sum([1 if item[gloss["source_ip"]] == line[gloss["source_ip"]] else 0 for item in data]) for line in data}
 
-print(get_counter_dict(load_data(DATA_PATH)))
+def get_port_to_protocol_mapping(data):
+    return {line[gloss["port"]]: line[gloss["protocol"]] for line in data}
+
+print(get_port_to_protocol_mapping(load_data(DATA_PATH)))
