e2c225668f687c0baf71941f673ee1272b46310b
Chaim <c0507634153@gmail.com>
2026-02-08 20:56:39 +0200

    add function that returns dict of addresses IP and their suspicions

--- analyzer.py
+++ analyzer.py
@@ -12,9 +12,11 @@ def get_unusual_size_ports(data) -> list:
     return [line for line in data if int(line[gloss['size']]) > LARGE_PACKET]
 
 def get_night_activity(data):
-    return [line for line in data if 0 < int(line[gloss["timestamp"]][11:13]) < 6]
+    return [line for line in data if (0 <= int(line[gloss["timestamp"]][11:13]) < 6)]
 
 def tag_bigger_ports(data):
     for line in data:
         line.append("LARGE" if int(line[gloss['size']]) > LARGE_PACKET else "NORMAL")
     return data
+
+
---
+++ checks.py
@@ -0,0 +1,18 @@
+from config import *
+from reader import *
+from analyzer import *
+from reporter import get_counter_dict
+
+def get_suspicions(data):
+    external_addresses = {log for log in get_external_addresses(data)}
+    night_activity = {log[gloss["source_ip"]] for log in get_night_activity(data)}
+    sensitive_port = {log[gloss["source_ip"]] for log in get_sensitive_port(data)}
+    large_ports = {log[gloss["source_ip"]] for log in get_unusual_size_ports(data)}
+    suspicions = {
+    "external_addresses": external_addresses,
+    "night_activity": night_activity,
+    "sensitive_ports": sensitive_port,
+    "large_ports": large_ports
+    }
+    return {log: [sus.upper() for sus in suspicions if log in suspicions[sus]] for log in get_counter_dict(data)}
+print(*get_suspicions(load_data(DATA_PATH)).items(), sep='\n')
