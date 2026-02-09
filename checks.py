bbb1a9771ae62de3e01add71e3f0a619db658e1b
Chaim <c0507634153@gmail.com>
2026-02-09 23:01:03 +0200

    add a function that filters log's suspicions and returns list of all its suspicions

--- checks.py
+++ checks.py
@@ -20,8 +20,10 @@ def get_addresses_with_2_suspicions(dict_suspects):
     return [pair for pair in dict_suspects.items() if len(pair[1]) > 1]
 
 suspicions_checks = {
-    "external_addresses": lambda log: log[gloss["source_ip"]] not in INTERNAL_ADDRESSES,
+    "external_addresses": lambda log: not log[gloss["source_ip"]].startswith(INTERNAL_ADDRESSES),
     "night_activity": lambda log: 0 <= int(log[gloss["timestamp"]][11:13]) < 6,
     "sensitive_ports": lambda log: log[gloss["port"]] in SENSITIVE_PORT,
-    "large_ports": lambda log: int(log[gloss["size"]] > LARGE_PACKET)
+    "large_ports": lambda log: int(log[gloss["size"]]) > LARGE_PACKET
 }
+def filter_suspicions(log):
+    return list(filter(lambda sus: suspicions_checks[sus](log), suspicions_checks))
