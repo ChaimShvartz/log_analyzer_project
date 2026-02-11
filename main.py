2719ff6dcda81b6b4c76986027a6565cb3ce3151
Chaim <c0507634153@gmail.com>
2026-02-11 12:22:01 +0200

    test stage-4

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from analyzer import *
 from checks import *
 from config import *
 
-data = gen_load_data(DATA_PATH)
-print(count_suspect_logs(data))
+# data = gen_load_data(DATA_PATH)
+lines = gen_load_data("network_traffic.log") # generator
+suspicious = gen_filter_suspect_logs(lines) # generator
+detailed = gen_suspicions_list_by_log(suspicious) # generator
+count = count_suspect_logs(suspicious)
+print(f"Total suspicious: {count}")
